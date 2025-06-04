9eaf28e8cfd6e67e7d4322cd8a3e49095e207d3f
Joe Zobel <jzobel19@yahoo.com>
2025-06-04 10:34:45 -0500

    update packages, more nvim cmp stuff

--- nvim/.config/nvim/lua/joe/lazy.lua
+++ nvim/.config/nvim/lua/joe/lazy.lua
@@ -15,7 +15,18 @@ local plugins = {
 	{ 'startup-nvim/startup.nvim' },
 
 	-- lsp, linting, and formatting
-	-- { 'github/copilot.vim' },
+	{ 'github/copilot.vim' },
+	{
+		"hrsh7th/nvim-cmp",
+		event = "InsertEnter",
+		dependencies = {
+			"hrsh7th/cmp-buffer", -- source for text in buffer
+			"hrsh7th/cmp-path", -- source for file system paths
+			"saadparwaiz1/cmp_luasnip", -- for autocompletion
+			"rafamadriz/friendly-snippets", -- useful snippets
+			"onsails/lspkind.nvim", -- vs-code like pictograms
+		}
+	},
 
 	-- themes
 	{ 'navarasu/onedark.nvim' },
